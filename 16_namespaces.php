b3342006951173f882a81a34f97b34b7570bac50
Sajib <samiul.islam.unisel@gmail.com>
2022-08-29 03:44:18 +0600

    This is basic object oriented PHP

---
+++ 16_namespaces.php
@@ -0,0 +1,28 @@
+<?php
+/* namespaces*/
+
+namespace html;
+
+class Table{
+    public $title;
+    public $numRows;
+
+    public function message() {
+        echo "<p>Table: '{$this->title}' has {$this->numRows} rows</p>";
+    }
+}
+$table = new Table;
+$table->title = "My table";
+$table->numRows = 4;
+
+?>
+
+<!doctype html>
+<html>
+<body>
+<?php
+$table->message();
+?>
+</body>
+</html>
+
